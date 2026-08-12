6fe274642a60bbe20b99d2b74433e177dd8354cf
Adham <adham612199@gmail.com>
2026-08-13 00:32:51 +0300

    Update project

--- frontend/src/components/AdminProductForm.jsx
+++ frontend/src/components/AdminProductForm.jsx
@@ -23,7 +23,7 @@ const CATEGORIES = [
   "Hair Care Set",
   "Protein Treatment",
   "Latest Offers",
-  "Handmade Bags",
+  "Handmade Bag",
 ];
 
 const AdminProductForm = ({ product, onCancel, onSubmit, saving }) => {
